2aa0062810b82e1a246fdaf5c5636e30a7411903
이우령 <wooryeongg@gmail.com>
2023-04-01 16:56:34 +0900

    component 구조 이해 및 구조 설정

---
+++ front-end/src/app/section/time-display/time-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-display',
+  templateUrl: './time-display.component.html',
+  styleUrls: ['./time-display.component.css']
+})
+export class TimeDisplayComponent {
+
+}
